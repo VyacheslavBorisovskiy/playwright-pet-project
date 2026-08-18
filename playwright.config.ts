8eb094ed94ad557ae5f81f58730f048a0461feb6
Viacheslav Borysovskyi <viacheslavborysovskyi@Viacheslavs-MacBook-Pro.local>
2026-08-18 13:49:24 +0200

    fix node version

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
-import { env } from './config/env';
 
 /**
  * Read environment variables from file.
@@ -25,13 +24,15 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['html'], ['junit', { outputFile: 'test-results/results.xml' }]]
+    : [['list'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: env.baseUrl,
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
