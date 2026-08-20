9f8755918d6edf2d0edc4a96aa0e38ae325bfff8
Viacheslav Borysovskyi <viacheslavborysovskyi@Viacheslavs-MacBook-Pro.local>
2026-08-20 15:16:01 +0200

    remove extra check

--- tests/api/login-api.spec.ts
+++ tests/api/login-api.spec.ts
@@ -15,9 +15,6 @@ test(
 
     expect(response.status()).toBe(200);
 
-    const contentType = response.headers()['content-type'];
-    expect(contentType).toContain('text/plain');
-
     const responseBody = await response.text();
 
     expect(responseBody).toContain('User exists!');
