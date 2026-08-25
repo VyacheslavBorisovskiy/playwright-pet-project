a93bafa092ddbaccf2d46b0bde111023f20965c0
Viacheslav Borysovskyi <viacheslavborysovskyi@Viacheslavs-MacBook-Pro.local>
2026-08-25 15:36:58 +0200

    add product to cart test

---
+++ tests/ui/addProduct.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '../../fixtures/baseFixture';
+
+test(
+  'User can add a product to the cart',
+  {
+    tag: '@smoke',
+  },
+  async ({ homePage, productsPage, productDetailsPage }) => {
+    await homePage.openProducts();
+    await productsPage.viewProduct();
+    await productDetailsPage.addToCart();
+    await productDetailsPage.continueShopping();
+
+    await expect(productDetailsPage.writeReviewLink).toBeVisible();
+  },
+);
